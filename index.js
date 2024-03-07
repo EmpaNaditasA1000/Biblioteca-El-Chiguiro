f9c10d2aa14bdbe2a6acc6eae7f6a630f9638ab9
EmpaNaditasA1000 <146375591+EmpaNaditasA1000@users.noreply.github.com>
2024-03-07 10:57:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("dbPiesDescalsos.json");
+const router = jsonServer.router("dbElChiguiro.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 30001;
 
